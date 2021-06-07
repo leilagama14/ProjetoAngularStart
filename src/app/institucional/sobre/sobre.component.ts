1b5fe7e5dbfc5db9d0979815308a9006376e7e94
leilagama14 <leilagama@gmail.com>
2021-06-07 12:17:07 -0300

    Alterações na aplicação para consumir o fake backend e exibir na tela de produtos.

--- src/app/institucional/sobre/sobre.component.ts
+++ src/app/institucional/sobre/sobre.component.ts
@@ -2,19 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
-  template: `
-    <p>
-      sobre works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './sobre.component.html',
+  styles: []
 })
 export class SobreComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
